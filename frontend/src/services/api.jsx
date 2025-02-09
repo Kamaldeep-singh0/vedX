ddd9d380d1f16c9c96f2384729a747dabf1293f4
Kamaldeep Singh <145911073+Kamaldeep-singh0@users.noreply.github.com>
2025-02-09 10:36:46 +0530

    env_of_fend

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -1,6 +1,8 @@
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 export const searchQuery = async (query) => {
     try {
-      const res = await fetch('http://localhost:5000/api/search/query', {
+      const res = await fetch(`${API_BASE_URL}/api/search/query`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
